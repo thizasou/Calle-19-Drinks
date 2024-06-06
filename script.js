be8dc12e159b7358448efaf28a42bc8dd3725932
Thiago Souza <thiagos1956@icloud.com>
2024-06-06 20:27:38 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('header a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        const headerOffset = 80;
+        const elementPosition = targetElement.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
